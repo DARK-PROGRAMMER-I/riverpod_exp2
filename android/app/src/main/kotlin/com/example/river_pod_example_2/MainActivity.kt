adce1fa4080f2b88825eb8f68d1f505551b40b24
NewUser <hammadmalli64@gmail.com>
2023-01-01 21:05:37 +0500

    Added Extension method

---
+++ android/app/src/main/kotlin/com/example/river_pod_example_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.river_pod_example_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+extension OptionalInfixAddition<T extends num>on T?{
+  T? operator +(T? other){
+    final shadow = this;
+    if(shadow != null){
+      return shadow + (other ?? 0) as T;
+    }else{
+      return null;
+    }
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Flutter Demo Home Page')
+      ),
+    );
+  }
+}
+
+
